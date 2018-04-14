df0412508de34e2457ca70d40654686d8f70bfa6
Panjoy Zhang <zyz864603980@hotmail.com>
2018-04-13 23:15:54 -0400

    add progress bar for delivery process

--- web/web-app/search/views.py
+++ web/web-app/search/views.py
@@ -7,11 +7,12 @@ def search(request):
 	if request.method == 'POST':
 		tnum = request.POST['tnum']
 		status=['created','truck on the way','waiting for warehouse load','out of delivery','delivered']
+		imgpath=['status1.png','status2.png','status3.png','status4.png','status5.png']
 		if shipments.objects.filter(shipment_id = tnum).exists():
 			shipment = shipments.objects.filter(shipment_id = tnum).all()[0]
 			message = status[shipment.status-1]
 			date = shipment.date.now().strftime("%Y-%m-%d %H:%M:%S")
-			return render(request,'search/shipmentdetail.html',context={'status':message,'date':date})
+			return render(request,'search/shipmentdetail.html',context={'status':message,'date':date,'imgpath':imgpath[shipment.status-1]})
 		else: 
 			return render(request,'search/invalid.html')
 	return HttpResponse('<h1>Not POST</h1>')
