86989462d8003087ac5e4daba41cb3edd6eae5ed
VCM admin account <vcm@vcm-3916.vm.duke.edu>
2018-04-24 14:24:09 -0400

    added main()

---
+++ asio_sample/servermain.cpp
@@ -0,0 +1,33 @@
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <string>
+#include <cstdlib>
+#include "ups.pb.h"
+#include "au.pb.h"
+#include "packedmessage.h"
+#include "main.cpp"
+
+
+using namespace std;
+namespace asio = boost::asio;
+
+void thread(asio::io_service & s) {
+  s.run();
+}
+
+int main(int argc, const char* argv[]) {
+  db::dbPointer db_ptr = db::create("upsweb", "postgres", "950703");
+  if (db_ptr->connect_db()) {
+    cerr << "Error when connecting to db\n";
+    return 1;
+  }
+  
+  asio::io_service io[2];
+  boost::shared_ptr<UpsServer> server_ptr = UpsServer::create(io[0], io[1], db_ptr);
+  server_ptr->start();
+  
+  boost::thread t{thread, io[0]};
+  io[1].run();
+  t.joint();
+  return 0;
+}
