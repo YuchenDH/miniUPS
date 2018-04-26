f3be6bde574e2b307f865b4c3413adcb3586007b
Zhang Yingzhuo <yz397@duke.edu>
2018-04-26 10:28:36 -0400

    .

--- web/ups_server/main.cpp
+++ web/ups_server/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, const char* argv[]) {
     cerr << "Error when connecting to db\n";
     return 1;
   }
-  
+  std::cout<<"db connected\r\n";
   asio::io_service io[2];
   boost::shared_ptr<UpsServer> server_ptr = UpsServer::create(io[0], io[1], db_ptr);
   server_ptr->start();
