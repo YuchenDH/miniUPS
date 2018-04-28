8d5dca01c5e9adb00670cf3127dd72800771f7b1
Panjoy Zhang <zyz864603980@hotmail.com>
2018-04-27 23:13:04 -0400

    .

--- web/web-app/weblog/views.py
+++ web/web-app/weblog/views.py
@@ -22,5 +22,14 @@ def index(request):
 
 def detail(request):
     uid = request.user.id
-    uos = orders.objects.filter(user_id = uid).all()
+    uos = orders.objects.filter(user_id = uid).all()   
+    if request.method == 'POST':
+        date = request.POST['date']
+        up = int(str(int(date)+1)+'0')
+        down = int(date+'0')
+        nuos=[]
+        for i in range(len(uos)):
+            if (uos[i].tracking_num < up) and (uos[i].tracking_num > down):
+                nuos.append(uos[i])
+        return render(request,'weblog/detail.html',context={'uos':nuos}) 
     return render(request,'weblog/detail.html',context={'uos':uos})
