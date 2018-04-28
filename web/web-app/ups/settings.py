f5aa729dccfa408ed2ae346a0f8894cd73010950
Zhang Yingzhuo <yz397@duke.edu>
2018-04-27 23:55:15 -0400

    .

--- web/web-app/ups/settings.py
+++ web/web-app/ups/settings.py
@@ -79,26 +79,26 @@ WSGI_APPLICATION = 'ups.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',#diff in localhost and docker //postgres
-#         #'PASSWORD': '950703',#delete in docker
-#         'USER': 'postgres',#
-#         'HOST': 'db',#diff in localhost and docker //db
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'upsweb',#diff in localhost and docker //postgres
-        'PASSWORD': '950703',#delete in docker
-        'USER': 'postgres',#
-        'HOST': 'localhost',#diff in localhost and docker //db
-        'PORT': '5432',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'postgres',#diff in localhost and docker //postgres
+         #'PASSWORD': '950703',#delete in docker
+         'USER': 'postgres',#
+         'HOST': 'db',#diff in localhost and docker //db
+         'PORT': '5432',
+     }
+ }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'upsweb',#diff in localhost and docker //postgres
+#        'PASSWORD': '950703',#delete in docker
+#        'USER': 'postgres',#
+#        'HOST': 'localhost',#diff in localhost and docker //db
+#        'PORT': '5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
